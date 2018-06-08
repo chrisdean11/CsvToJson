ba85aa331239295bd739e1c000e3747f428016ff
cd <christopherjohndean@gmail.com>
2018-06-08 00:10:44 -0700

    Working now

--- CsvToJson.py
+++ CsvToJson.py
@@ -7,27 +7,23 @@ import glob
 def getIndent(line):
 	ind = 0
 	for str in line:
-		if (str is None):
+		if (str.strip() == ''):
 			ind = ind + 1
 		else:
-			print(line, " -- indent: ", ind)
 			return ind
-	print(line, " -- indent: -1")
 	return -1
 
 # Returns the number of non-empty values on this line of csv. Assumes there are no empty strings in-between.
 def count(line):
 	count = 0
-	print(line)
 	for str in line:
-		if (str.strip()):
+		if (str.strip() != ''):
 			count = count + 1
-	print("  ",line," count: ", count)
 	return count
 
 # Returns whitespace to indent the output file
 def indent(num):
-	result = ""
+	result = "  "
 	for i in range(num):
 		result += "  "
 	return result
@@ -50,43 +46,69 @@ def getValAtColumn(line, indent):
 	else:
 		return "\"" + line[indent] + "\""
 
+def stripComma(res):
+	# Special case of last character 
+	if res[-1] == ',':
+		return res[:-1]
+	for i in range(1, len(res)):
+		# If the last line was the end of an object, it won't have a comma at the end
+		if res[-i] == '}':
+			return res
+		if res[-i] == ',':
+			return res[:-i] + res[-i+1:]
+
 def main():
 	with open (sys.argv[1]) as file:
 		res = "{\n"
 		ind = 0
 
 		while (True):
-			line = file.readline()
+			line = file.readline().rstrip('\n')
+			if not line or line.strip() == '':
+				break
 			line = line.split(',')
+			lineIndent = getIndent(line)
 
 			# End of an object
-			if (getIndent(line) < ind):
+			if (lineIndent < ind):
 				ind = ind - 1
-				res += indent(ind) + "}\n"
+				res = stripComma(res)
+				res += indent(ind) + "},\n"
 
 			# Beginning of new object
 			if (count(line) == 1):
-				res += "\"" + getValAtColumn(line, getIndent(line)) + "\" : {\n"
+				res += indent(ind) + getValAtColumn(line, lineIndent) + " : \n" + indent(ind) + "{\n"
 				ind = ind + 1
 
 			# Key-Value pair
 			elif (count(line) == 2):
-				res += "\"" + line[getIndent(line)] + "\" : " + getValAtColumn(line, getIndent(line) + 1) + ",\n"
+				res += indent(ind) + "\"" + line[lineIndent] + "\" : " + getValAtColumn(line, lineIndent + 1) + ",\n"
 
 			# Array
 			elif (count(line) > 2):
-				res += "\"" + line[getIndent(line)] + "\" : ["
-				for i in range(getIndent(line) + 1, getIndent(line)+count(line)):
+				res += indent(ind) + "\"" + line[lineIndent] + "\" : ["
+				for i in range(lineIndent + 1, lineIndent + count(line)):
 					res += getValAtColumn(line, i) 
-					if i != getIndent(line) + count(line): res += ", "
-				res += "]\n"
+					if i != lineIndent + count(line) - 1: 
+						res += ", "
+				res += "],\n"
 
-			# Blank line at end or error
+			# 
 			else:
 				break
 
+		# Get rid of last comma added
+		res = stripComma(res)
 		res += "}"
 		print (res)
+
+		# Write to file
+		base = os.path.basename(sys.argv[1])
+		filename = os.path.splitext(base)[0] + '.json'
+		print(filename)
+		resFile = open(filename, "w")
+		resFile.write(res)
+		resFile.close()
 
 if __name__ == "__main__":
 	print('executing CsvToJson')
